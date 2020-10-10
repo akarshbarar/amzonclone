b0fb73b0dbe828ffaa9dd447bd44daad2b66d756
Akarsh Barar <barar.akarsh8@gmail.com>
2020-10-10 16:02:49 +0530

    Key Files added

---
+++ pages/cancelled.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <Header/>
+      <HeaferTwo/>
+      <h1>Order cancelled</h1>
+  </div>
+</template>
+
+<script>
+
+import Header from '../layouts/partials/Navbar'
+import HeaderTwo from '../layouts/partials/HeaderTwo'
+export default {
+components:{
+    Header,
+    HeaderTwo
+
+  },
+}
+</script>
+
+<style>
+
+</style>
